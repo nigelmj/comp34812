f37193ce1a12abfed83eed576ef4a041a6586c2d
amitrajitpaticode <patiamitrajit@gmail.com>
2025-04-03 22:09:52 +0100

    Model card completed

--- model_card_generation.py
+++ model_card_generation.py
@@ -19,15 +19,15 @@ card = ModelCard.from_template(
     model_id = 'p64932nj-y24592ap-NLI',
     model_summary = '''This is a classification model that, given a premise and a hypothesis,
       was trained to determine whether the hypothesis is true based on the premise.''',
-    model_description = '''This model is based upon a BERT model that was fine-tuned
-      on 30K pairs of texts.''',
+    model_description = '''This model is based upon a RoBerta model that was fine-tuned
+      on 30K pairs of texts + synthetically generated texts.''',
     developers = 'Nigel Jose and Amitrajit Pati',
     base_model_repo = 'https://huggingface.co/google-bert/bert-base-uncased',
     base_model_paper = 'https://aclanthology.org/N19-1423.pdf',
     model_type = 'Supervised',
     model_architecture = 'Transformers',
     language = 'English',
-    base_model = 'bert-base-uncased',
+    base_model = 'roberta-base',
 
     # TODO: fill in the following attributes with the appropriate values
 
@@ -37,29 +37,32 @@ card = ModelCard.from_template(
       - train_batch_size: 16
       - eval_batch_size: 16
       - seed: 42
-      - num_epochs: 10''',
+      - num_epochs: 1
+      - warmup_ratio: 0.1
+      - weight_decay: 0.01''',
     speeds_sizes_times = '''
-      - overall training time: 5 hours
-      - duration per training epoch: 30 minutes
-      - model size: 300MB''',
-    testing_data = 'A subset of the development set provided, amounting to 2K pairs.',
+      - overall training time: 40 minutes
+      - duration per training epoch: 15 minutes
+      - model size: 500.78MB''',
+    testing_data = 'A testing file provided to us, called dev.csv'
     testing_metrics = '''
-      - Precision
-      - Recall
-      - F1-score
-      - Accuracy''',
-    results = 'The model obtained an F1-score of 67% and an accuracy of 70%.',
+      - Precision = 0.86162742994469
+      - Recall = 0.86163895486936
+      - F1-score = 0.86163045534955
+      - Accuracy = 0.86163895486936
+      ''',
+    results = 'The model obtained an F1-score of 86% and an accuracy of 86%.',
     hardware_requirements = '''
       - RAM: at least 16 GB
-      - Storage: at least 2GB,
-      - GPU: V100''',
+      - Storage: at least 1GB,
+      - GPU: P100''',
     software = '''
       - Transformers 4.18.0
       - Pytorch 1.11.0+cu113''',
     bias_risks_limitations = '''Any inputs (concatenation of two sequences) longer than
-      512 subwords will be truncated by the model.''',
+      256 subwords will be truncated by the model.''',
     additional_information = '''The hyperparameters were determined by experimentation
-      with different values.'''
+      with different values. And additional data addition technique was from the paper :Arxiv.org. (2015). Improving the Natural Language Inference robustness to hard dataset by data augmentation and preprocessing. [online] Available at: https://arxiv.org/html/2412.07108v1#S3.'''
 )
 
 # the following lines will write a markdown (.md) file; this becomes one of your model cards
